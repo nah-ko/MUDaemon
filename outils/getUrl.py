5bcaccc3a8f90ae9f796d564a29e8bc04c9d88c0
nah-ko <>
2003-10-12 17:31:48 +0000

    Work now with both file or directory.

--- outils/getUrl.py
+++ outils/getUrl.py
@@ -7,10 +7,94 @@
 
 import sys, os, time, signal, socket
 
-def convert_dir(directory, action, outfile, verbosity=false):
+def convert_dir(directory, action, outfile, verbosity='False'):
     ''' Converting directory recursivly into asked mode '''
 
-def convert_file(infile, action, outfile, verbosity=false):
+    # Init vars
+    result    = ''
+    last_ccat = last_pcat = ''
+    saw_cat   = []
+    DLopen    = DLclose = 0
+
+    # Getting Category and ParentCategory from path
+    if verbosity:
+    	print "Working directory: %s" % directory
+
+    # Open outfile in creation mode
+    fp = open(outfile, 'w')
+
+    # Change to working dir
+    os.chdir(directory)
+
+    # Do parsing
+    for root, dirs, files in os.walk('.'):
+	    for name in files:
+		    dir, file = os.path.split(os.path.join(root, name))
+		    if len(dir.split('/')) == 1:
+			current_cat = 'Other'
+			parent_cat  = None
+		    else:
+			[parent_cat, current_cat] = dir.split('/')[-2:]
+			if parent_cat == '.':
+			    parent_cat = 'Other'
+		    if verbosity:
+		    	print "File: %s\nCurrent category: %s\nParent category: %s (%s)" % (file, current_cat, parent_cat, os.path.join(root, name))
+			
+		    # Fetching datas
+		    urlname, link, category = read_file(os.path.join(root, name), verbosity)
+
+		    # Moving datas according to action
+		    if action == 'sql':
+		    	result += "INSERT (Link, UrlName, Category, ParentCategory) INTO favoris VALUES ('%s', '%s', '%s', '%s')\n" % (link, urlname, current_cat, parent_cat)
+		    elif action == 'html':
+		        if result == "": # only one time here, first pass
+			    result    += "<DL>\n<DT> %s\n" % current_cat
+			    DLopen    += 1
+			    last_ccat = current_cat
+			    last_pcat = parent_cat
+		            saw_cat.append(parent_cat)
+			    if verbosity:
+			        print "## First ## Current: %s Parent: %s Seen: %s" % (current_cat, parent_cat, saw_cat)
+			elif current_cat == last_ccat: # link into a category
+			    result    += "<DD><a href=\"%s\"> %s </a>\n" % (link, urlname)
+			    last_ccat = current_cat
+			    last_pcat = parent_cat
+			    if verbosity:
+			        print "## Links ## Current: %s Parent: %s Seen: %s" % (current_cat, parent_cat, saw_cat)
+			elif current_cat != last_ccat: # new category
+			    if parent_cat == last_ccat: # it's a sub category
+			        result    += "<DL>\n<DT> %s\n" % current_cat
+			        DLopen    += 1
+			        last_ccat = current_cat
+			        last_pcat = parent_cat
+		                saw_cat.append(parent_cat)
+			        if verbosity:
+			            print "## New sub cat ## Current: %s Parent: %s Seen: %s" % (current_cat, parent_cat, saw_cat)
+			    elif parent_cat != last_ccat: # new category in same parent category
+			        if parent_cat in saw_cat:
+				    while saw_cat[-1] != parent_cat:
+				    	if verbosity:
+				            print "Remove: %s" % saw_cat[-1]
+				    	result  += "</DL>\n"
+				    	DLclose += 1
+					saw_cat.remove(saw_cat[-1])
+			        result    += "</DL>\n<DL>\n<DT> %s\n" % current_cat
+			        DLopen    += 1
+			        DLclose   += 1
+			        last_ccat = current_cat
+			        last_pcat = parent_cat
+			        if verbosity:
+			            print "## DD new cat ## Current: %s Parent: %s Seen: %s" % (current_cat, parent_cat, saw_cat)
+    		    if verbosity:
+    			print "Resultat: %s" % result
+
+    # Write datas
+    fp.write(result)
+    # Close file
+    fp.close()
+
+
+def convert_file(infile, action, outfile, verbosity='False'):
     ''' Converting datas into asked mode '''
 
     # Single file, consider ParentCategory not existent
@@ -26,7 +110,7 @@ def convert_file(infile, action, outfile, verbosity=false):
     if action == 'sql':
     	result = "INSERT (Link, UrlName, Category, ParentCategory) INTO favoris VALUES ('%s', '%s', '%s', '%s')" % (link, urlname, category, pcategory)
     elif action == 'html':
-    	result = "%s - <a href=\"%s\"> %s </a>" % (category, urlname, link)
+    	result = "<DL>\n<DT>%s\n<DD><a href=\"%s\"> %s </a>\n</DL>" % (category, urlname, link)
     if verbosity:
     	print "Resultat: %s" % result
     # Open outfile in creation mode
@@ -34,7 +118,7 @@ def convert_file(infile, action, outfile, verbosity=false):
     file.write(result)
     file.close()
 
-def read_file(filename, verbosity=false):
+def read_file(filename, verbosity='False'):
     ''' Reading url file '''
 
     import ConfigParser
@@ -51,7 +135,7 @@ def read_file(filename, verbosity=false):
         print "Category: %s" % Category
 
     # Name of link
-    Linkname, Extension = os.path.basename(filename).split('.')
+    Linkname = os.path.basename(filename).split('.url')[-2]
     if verbosity:
     	print "Link name: %s" % Linkname
 
@@ -91,21 +175,23 @@ def main():
     elif options.HTML:
         action = "html"
 
-    # Set the type of «favorite» argument
+    # output filename creation (made with action)
+    outputFile = args[1] + '.' + action
+    if options.verbose:
+    	print "Output file: %s" % outputFile
+
+    # Set the type of "favorite" argument
     if os.path.isdir(args[1]):
     	srcType = 'dir'
     	if options.verbose:
 		print "Going to convert %s %s to %s file named %s.%s" % (args[0], srcType, action, args[1], action)
+	convert_dir(args[1], action, outputFile, options.verbose)
     elif os.path.isfile(args[1]):
     	# It's a single file
     	srcType = 'file'
-    	# output filename creation (made with action)
-    	outputFile = args[1] + '.' + action
-    	if options.verbose:
-    		print "Output file: %s" % outputFile
-    	convert_file(args[0], action, outputFile, options.verbose)
     	if options.verbose:
 		print "Going to convert %s %s to %s file named %s.%s" % (args[0], srcType, action, args[1], action)
+    	convert_file(args[0], action, outputFile, options.verbose)
 
 
 
